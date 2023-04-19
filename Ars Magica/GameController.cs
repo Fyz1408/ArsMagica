4cc241f487e64d50110f1617005c8f2c1dbd0d3f
Mads Frandsen <frandsen_34@hotmail.dk>
2023-04-19 13:30:37 +0200

    Fight Now V0.5

--- Ars Magica/GameController.cs
+++ Ars Magica/GameController.cs
@@ -6,30 +6,99 @@ using System.Threading.Tasks;
 
 namespace Ars_Magica
 {
-  public class GameController
-  {
-    private List<Armor> ListOfArmor;
-    private List<Weapon> ListOfWeapons;
-    public Combatant C1;
-    public Combatant C2;
-
-    public GameController()
+    public class GameController
     {
-      ListOfArmor = Armor.ArmorList();
-      ListOfWeapons = Weapon.WeaponList();
-      Armor a = new Armor();
-      Weapon w = new Weapon();
+        private List<Armor> ListOfArmor;
+        private List<Weapon> ListOfWeapons;
+        public Combatant C1;
+        public Combatant C2;
 
-      C1 = new Combatant(a.GetRandomArmor(), w.GetRandomWeapon());
-      C2 = new Combatant(a.GetRandomArmor(), w.GetRandomWeapon());
+        public GameController()
+        {
+            ListOfArmor = Armor.ArmorList();
+            ListOfWeapons = Weapon.WeaponList();
+            Armor a = new Armor();
+            Weapon w = new Weapon();
+
+            C1 = new Combatant(a.GetRandomArmor(), w.GetRandomWeapon());
+            C2 = new Combatant(a.GetRandomArmor(), w.GetRandomWeapon());
+        }
+
+        public Combatant Fight()
+        {
+            int c1Roll = RND.Roll();
+            int c2Roll = RND.Roll();
+
+            return c1Roll > c2Roll ? C1 : C2;
+        }
+        public Combatant FightNow()
+        {
+            Combatant Winner;
+
+            double c1Advantage = 0;
+            double c2Advantage = 0;
+
+            double c1AtackTotal;
+            double c1DefenseTotal;
+            double c2AtackTotal;
+            double c2DefenseTotal;
+
+            double c1Atack = 0;
+            double c2Atack = 0;
+
+            Stats C1Stat = Stat(C1);
+            Stats C2Stat = Stat(C2);
+
+
+            while (C1.Hp > 0 & C2.Hp > 0)
+            {
+                int c1Roll = RND.Roll();
+                int c2Roll = RND.Roll();
+
+                c1AtackTotal = C1Stat.ATK + c1Roll + c1Advantage;
+                c1DefenseTotal = C1Stat.DFN + c1Roll + c1Advantage;
+                c2AtackTotal = C2Stat.ATK + c2Roll + c2Advantage;
+                c2DefenseTotal = C2Stat.DFN + c2Roll + c2Advantage;
+
+                c1Atack = c1AtackTotal - c2DefenseTotal;
+                c2Atack = c2AtackTotal - c1DefenseTotal;
+
+                if (c1Atack > c2Atack)
+                {
+                    if (c1Atack <1)
+                    {
+                        c1Atack = 1;
+                    }
+                    c1Advantage = c1Atack;
+                }
+                if (c1Atack < c2Atack)
+                {
+                    if (c2Atack < 1)
+                    {
+                        c2Atack = 1;
+                    }
+                    c2Advantage = c2Atack;
+                }
+
+
+
+            }
+
+
+            return Winner;
+        }
+        private Stats Stat(Combatant combatant)
+        {
+            Stats stats = new Stats();
+            double enc = 1 + combatant.Weapon.Load + combatant.Armor.Load;
+            stats.ENC = enc;
+            stats.INIT = 1 + 1 + 1 + enc;
+            stats.ATK = 1 + 1 + combatant.Weapon.Atk + enc;
+            stats.DFN = 1 + 1 + combatant.Weapon.Dfn - 1 + enc;
+            stats.SOAK = 1 + 1 + combatant.Armor.Prot;
+
+            return stats;
+        }
+
     }
-
-    public Combatant Fight()
-    {
-      int c1Roll = RND.Roll();
-      int c2Roll = RND.Roll();
-
-      return c1Roll > c2Roll ? C1 : C2;
-    }
-  }
 }
---
+++ Ars Magica/Stats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ars_Magica
+{
+    internal class Stats
+    {
+        public double ENC { get; set; }
+        public double INIT { get; set; }
+        public double ATK { get; set; }
+        public double DFN { get; set; }
+        public double SOAK { get; set; }
+    }
+}
