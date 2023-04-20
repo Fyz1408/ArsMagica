6d692baff74a0128a1013fd2ebf8ede61f427b7d
Mads Frandsen <frandsen_34@hotmail.dk>
2023-04-20 09:31:50 +0200

    FigthNow done

--- Ars Magica/GameController.cs
+++ Ars Magica/GameController.cs
@@ -6,28 +6,28 @@ using System.Threading.Tasks;
 
 namespace Ars_Magica
 {
-  public class GameController
-  { 
-    private List<Combatant> Combatants;
-    private List<Armor> ListOfArmor;
-    private List<Weapon> ListOfWeapons;
-    public Combatant C1;
-    public Combatant C2;
+    public class GameController
+    {
+        private List<Combatant> Combatants;
+        private List<Armor> ListOfArmor;
+        private List<Weapon> ListOfWeapons;
+        public Combatant C1;
+        public Combatant C2;
 
-    public GameController()
-    {
-      Combatants = new List<Combatant>();
-      C1 = new Combatant("Lars", Armor.ArmorList()[RND.Range(0, 9)], Weapon.WeaponList()[RND.Range(0, 7)]);
-      C2 = new Combatant("Maximilianus", Armor.ArmorList()[RND.Range(0, 9)], Weapon.WeaponList()[RND.Range(0, 7)]);
-      
-      Combatants.Add(C1);
-      Combatants.Add(C2);
-    }
-    
-    public Combatant? FindCombatantFromName(string name)
-    {
-        return Combatants.Find(c => c.Name.ToLower() == name.ToLower());
-    }
+        public GameController()
+        {
+            Combatants = new List<Combatant>();
+            C1 = new Combatant("Lars", Armor.ArmorList()[RND.Range(0, 9)], Weapon.WeaponList()[RND.Range(0, 7)]);
+            C2 = new Combatant("Maximilianus", Armor.ArmorList()[RND.Range(0, 9)], Weapon.WeaponList()[RND.Range(0, 7)]);
+
+            Combatants.Add(C1);
+            Combatants.Add(C2);
+        }
+
+        public Combatant? FindCombatantFromName(string name)
+        {
+            return Combatants.Find(c => c.Name.ToLower() == name.ToLower());
+        }
 
         public Combatant Fight()
         {
@@ -38,7 +38,7 @@ namespace Ars_Magica
         }
         public Combatant FightNow()
         {
-            Combatant Winner;
+            Combatant Winner =C1;
 
             double c1Advantage = 0;
             double c2Advantage = 0;
@@ -54,14 +54,49 @@ namespace Ars_Magica
             Stats C1Stat = Stat(C1);
             Stats C2Stat = Stat(C2);
 
+            double c1FullHP = C1Stat.SOAK + C1.Hp;
+            double c2FullHP = C2Stat.SOAK + C2.Hp;
+
+            int figth =0;
 
             while (C1.Hp > 0 & C2.Hp > 0)
             {
+                Winner = C1;
+                int Rolldice = 0;
+                int doublenumber = 2;
                 int c1Roll = RND.Roll();
-                int c2Roll = RND.Roll();
+                while (c1Roll == 1 || Rolldice == 1)    //c1r =1
+                {
+                    Rolldice = RND.Roll();          //rd = 1
+                    if (Rolldice == 1)
+                    {
+                        doublenumber = doublenumber * 2;
+                    }
+                    else
+                    {
+                        c1Roll = Rolldice * doublenumber;
+                    }
 
-                c1AtackTotal = C1Stat.ATK + c1Roll + c1Advantage;
-                c1DefenseTotal = C1Stat.DFN + c1Roll + c1Advantage;
+                }
+                Rolldice = 0;
+                doublenumber = 2;
+                int c2Roll = RND.Roll();
+                while (c2Roll == 1 || Rolldice == 1)    //c1r =1
+                {
+                    Rolldice = RND.Roll();          //rd = 1
+                    if (Rolldice == 1)
+                    {
+                        doublenumber = doublenumber * 2;
+                    }
+                    else
+                    {
+                        c2Roll = Rolldice * doublenumber;
+                    }
+
+                }
+                //AtackTotal = Atk stat + dice roll + left over AtackTotal + advantage  
+                c1AtackTotal = C1Stat.ATK + c1Roll + c1Advantage; //4+3+0 first round   = 7
+                c1DefenseTotal = C1Stat.DFN + c1Roll + c1Advantage;         //6+3+0   first round   = 9
                 c2AtackTotal = C2Stat.ATK + c2Roll + c2Advantage;
                 c2DefenseTotal = C2Stat.DFN + c2Roll + c2Advantage;
 
@@ -70,11 +105,13 @@ namespace Ars_Magica
 
                 if (c1Atack > c2Atack)
                 {
-                    if (c1Atack <1)
+                    if (c1Atack < 1)
                     {
                         c1Atack = 1;
                     }
                     c1Advantage = c1Atack;
+                    if (c1Advantage < 0) { c1Advantage = 0; }
+                    c2Advantage = 0;
                 }
                 if (c1Atack < c2Atack)
                 {
@@ -83,12 +120,25 @@ namespace Ars_Magica
                         c2Atack = 1;
                     }
                     c2Advantage = c2Atack;
+                    if (c2Advantage < 0) { c2Advantage = 0; }
+                    c1Advantage = 0;
                 }
-
-
-
+                //soak dam bliver taget ved at tage total Atack dam og / det med 5 så fpr at 
+                double c1AtackDamage = c1AtackTotal / 5;
+                if (c1AtackDamage >= c2FullHP)
+                {
+                    C2.Hp = 0;
+                    c1AtackTotal = 0;
+                    Winner = C1;
+                }
+                double c2AtackDamage = c2AtackTotal / 5;
+                if (c2AtackDamage >= c1FullHP)
+                {
+                    C1.Hp = 0;
+                    c2AtackTotal = 0;
+                    Winner = C2;
+                }
             }
-
 
             return Winner;
         }
